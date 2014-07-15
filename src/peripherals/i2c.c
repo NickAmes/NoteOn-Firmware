67297e4277baca3ee02b861aff1fb27fbf240620
Nick Ames <nick@fetchmodus.org>
2014-07-15 16:51:42 -0700

    Added i2c.c/.h

---
+++ src/peripherals/i2c.c
@@ -0,0 +1,7 @@
+/* I2C Setup and Control Functions
+ * This file provides a high-level interface to the I2C1 peripheral.
+ *
+ * This file is a part of the firmware for the NoteOn Smartpen.
+ * Copyright 2014 Nick Ames <nick@fetchmodus.org>. Licensed under the GNU GPLv3.
+ * Contains code from the libopencm3 project.                              */
+#include "i2c.h"
