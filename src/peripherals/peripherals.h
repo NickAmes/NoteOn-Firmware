402851f6bdb2e20f5ad11f43ded194c696337b13
Nick Ames <nick@fetchmodus.org>
2014-07-15 03:24:59 -0700

    Moved peripheral included to a single file.

---
+++ src/peripherals/peripherals.h
@@ -0,0 +1,14 @@
+ /* All-peripherals include file.
+  *
+  * This file is a part of the firmware for the NoteOn Smartpen.
+  * Copyright 2014 Nick Ames <nick@fetchmodus.org>. Licensed under the GNU GPLv3.
+  * Contains code from the libopencm3 and newlib projects.                    */
+#ifndef PERIPHERALS_H
+#define PERIPHERALS_H
+
+#include "led.h"
+#include "clock.h"
+#include "systick.h"
+#include "usart.h"
+
+#endif
