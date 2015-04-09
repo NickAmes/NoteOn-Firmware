db86d3bc6ad0521cb2be92416ead062addbffbd2
Nick Ames <nick@fetchmodus.org>
2015-04-08 18:51:47 -0700

    A bit of work on the bluetooth driver.

--- src/board/bluetooth.c
+++ src/board/bluetooth.c
@@ -15,9 +15,37 @@
 #define REQn_high() (GPIOA_BSRR = GPIO7)
 #define REQn_low() (GPIOA_BSRR = (GPIO7 << 16))
 
+/* If true, the memory driver has control of the SPI peripheral. */
+static bool GotSPI;
+
+/* request_spi() callback. Simply sets GotSPI to true. */
+static void request_spi_callback(void){
+	GotSPI = true;
+}
+
+/* Get control of the SPI bus (by waiting until it's available) and perform
+ * spi setup. */
+static void get_spi(void){
+	request_spi(request_spi_callback);
+	while(!GotSPI){
+		/* Wait */
+	}
+	setup_spi(0, 0, SPI_CR1_BAUDRATE_FPCLK_DIV_16, SPI_CR1_LSBFIRST);
+}
+
+/* Release the SPI bus and set GotSPI to 0. */
+static void release_spi_bt(void){
+	GotSPI = false;
+	release_spi();
+}
+
 /* Initialize the nRF8001 Bluetooth transceiver.
  * Returns 0 on success, -1 on error. */
-int init_bt(void);
+int init_bt(void){
+	gpio_mode_setup(GPIOB, GPIO_MODE_INPUT, GPIO_PUPD_PULLUP, GPIO0); /* RDYn */
+	REQn_high();
+	gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO7); /* REQNn */
+	
 
 /* Put the Bluetooth transceiver into a low power state. */
 void shutdown_bt(void);
